56b428647a1ae72d1a40e8f860ac242408a53416
David Enikuomehin <enikuauth@gmail.com>
2025-04-19 18:05:52 +0100

    fix: clear CSP middleware

--- pygoat/middleware.py
+++ pygoat/middleware.py
@@ -1,12 +1,3 @@
-class ContentSecurityPolicyMiddleware:
-      def __init__(self, get_response):
-          self.get_response = get_response
-
-      def __call__(self, request):
-          response = self.get_response(request)
-          response["Content-Security-Policy"] = "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; frame-ancestors 'none'; form-action 'self';"
-          return response
-
 class HSTSHeaderMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
